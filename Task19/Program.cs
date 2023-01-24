4b50945200239d453ba1b31e1546f197853de2a7
Andrey <chizhov67@gmail.com>
2023-01-24 22:24:18 +0300

    Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,21 @@
+﻿int N;
+
+Console.Write("Введите пятизначное число: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+if (N / 10000 == N % 10 && N % 10000 / 1000 == N % 100 / 10)
+	{
+		Console.WriteLine("Да");
+    }
+else
+			
+	{
+		Console.WriteLine("Нет");
+	}	
+
+	
+        Console.ReadLine();
+        
+		
+	
+
