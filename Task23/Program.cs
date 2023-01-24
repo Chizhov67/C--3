b8cbd502c378aee511adb534b1fb912181481409
Andrey <chizhov67@gmail.com>
2023-01-24 23:11:38 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTask23
+{
+	internal class Program
+	{
+		static int calcCube(int n)
+		{
+			return Convert.ToInt32(Math.Pow(n, 3));
+		}
+		static void Main(string[] args)
+		{
+			int N;
+
+			Console.Write("Введите число: ");
+			N = Convert.ToInt32(Console.ReadLine());
+
+			for (int i = 1; i < N + 1; i++)
+			{
+				Console.Write(calcCube(i) + " ");
+			}
+
+			Console.ReadLine();
+
+		}
+	}
+}
